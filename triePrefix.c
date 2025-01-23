f184dfb61eb39bc5f66d89956435ee2cb0c0e4d8
kamillamamatova <kamillamamatova.km@gmail.com>
2025-01-23 01:40:24 -0500

    Update triePrefix.c

--- triePrefix.c
+++ triePrefix.c
@@ -1,3 +1,8 @@
+/* This code manages vehicle owners and their fines. The system keeps track 
+of owner names, their fines, and their position in the tree. It provides 
+functionalities for adding, searching, deducting, deleting, calculating, 
+and balancing nodes (representing owners). */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
